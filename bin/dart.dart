856c7959edda50f960acc5ea62091a5d05efd918
Tony <anthony.mcclay@me.com>
2020-06-13 07:51:16 -0400

    until code 2.2

--- bin/dart.dart
+++ bin/dart.dart
@@ -3,17 +3,55 @@
 //}
 
 //code 2.1
-void main(List<String> arguments) {
+//void main(List<String> arguments) {
+//  bool firstButtonTouch = true;
+//  bool secondButtonTouch = false;
+//  bool thirdButtonTouch = true;
+//  bool fourthButtonTouch = false;
+//  if (firstButtonTouch)
+//    print("The giant starts running.");
+//  else
+//    print("To stop the giant please touch the second button.");
+//
+//  if (secondButtonTouch)
+//    print("The giant stops.");
+//  else
+//    print("You have not touched the second button.");
+//
+//  print("Touch any button to start the game.");
+//
+//  if (thirdButtonTouch)
+//    print("The giant goes to sleep.");
+//  else
+//    print("You have not touched any button.");
+//
+//  if (fourthButtonTouch)
+//    print("The giant wakes up.");
+//  else
+//    print("You have not touched any button.");
+//}
+
+//code 2.2
+main(List<String> arguments) {
   bool firstButtonTouch = true;
+  var firstButtonUntouch;
   bool secondButtonTouch = false;
   bool thirdButtonTouch = true;
   bool fourthButtonTouch = false;
-  if (firstButtonTouch)
-    print("The giant starts running.");
-  else
-    print("To stop the giant please touch the second button.");
+  firstButtonUntouch ??= firstButtonTouch;
+  firstButtonUntouch = false;
 
-  if (secondButtonTouch)
+  if (firstButtonUntouch == false || firstButtonTouch == true)
+    print("The giant is sleeping.");
+  else
+    print("You need to wake up the giant. Touch the first button.");
+
+  if (firstButtonTouch == true && firstButtonUntouch == false)
+    print("The giant starts running.");
+  print("To stop the giant please touch the second button.");
+
+  if ((secondButtonTouch == true && thirdButtonTouch == true) ||
+      fourthButtonTouch == false)
     print("The giant stops.");
   else
     print("You have not touched the second button.");
@@ -30,4 +68,3 @@ void main(List<String> arguments) {
   else
     print("You have not touched any button.");
 }
-
